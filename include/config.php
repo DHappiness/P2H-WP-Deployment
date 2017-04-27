16a1966c1d8c86a71ee9b4fac31b4138bcfeedb1
DenisYakimchuk <stakanblin@list.ru>
2017-04-27 19:40:13 +0300

    Update config.php

--- include/config.php
+++ include/config.php
@@ -2,8 +2,8 @@
 global $deployment_settings;
 
 $deployment_settings = array(
-  'svn_username' => '',
-  'svn_password' => '',
+  'svn_username' => '', /* required */
+  'svn_password' => '', /* required */
   'install_plugins' => array( 'acf'  ),
   'deployment_version' => '0.3' /* Don not change */
 );
