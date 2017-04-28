64b3a0e61ec1f1d1af88a4b5e7813a76a5dc630e
DenisYakimchuk <stakanblin@list.ru>
2017-04-28 11:36:54 +0300

    Delete config.php

--- include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $deployment_settings;
-
-$deployment_settings = array(
-  'svn_username' => '', /* required */
-  'svn_password' => '', /* required */
-  'install_plugins' => array( 'acf'  ),
-  'deployment_version' => '0.3' /* Don not change */
-);
