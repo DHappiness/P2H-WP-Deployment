4f6fba5359bbc4585e9f550a8082f82f0a30ef27
DenisYakimchuk <stakanblin@list.ru>
2017-06-28 10:09:50 +0300

    Update settings-form.php

--- include/settings-form.php
+++ include/settings-form.php
@@ -8,27 +8,36 @@
 </head>
 <body>
 <div class="main-settings">
-  <form action="./deployment.php" method="post" enctype="multipart/form-data">
+  <form action="./deployment.php" method="post" enctype="multipart/form-data" class="form-validation">
     <fieldset>
       <legend>MySQL Settings</legend>
-      <input type="text" name="dbname" size="34" placeholder="DB name + user name ( they are the same )" />
+      <input type="text" name="dbname" data-required="true" size="34" placeholder="DB name + user name ( they are the same )" />
       <input type="text" name="dbuser" placeholder="DB User" />
-      <input type="text" name="password" placeholder="Password" />
+      <input type="text" name="password" data-required="true" placeholder="Password" />
     </fieldset>
     <fieldset>
       <legend>Wordpress Settings</legend>
-      <input type="text" name="sitename" placeholder="Site Name" />
+      <input type="text" name="sitename" data-required="true" placeholder="Site Name" />
+    </fieldset>
+    <fieldset>
+      <legend>Create CPTs</legend>
+      <div class="clone-elements" data-depth="1" data-subitems="taxonomies">
+        <a href="#" class="add-item">Add CPT</a>
+        <br /><br />
+        <div class="item">
+          <input type="text" name="cpt[0][title]" placeholder="Title (Singular)">
+          <a href="#" class="add-subitem">Add Taxonomy</a>
+        </div>
+      </div>
     </fieldset>
     <fieldset>
       <legend>Create Pages</legend>
-      <div class="clone-elements">
-        <div class="pages">
-          <a href="#" class="add-page">Add Page</a>
-          <br /><br />
-          <div class="page">
-            <input type="text" name="pages[0][title]" placeholder="Page Title">
-            <a href="#" class="add-subpage">Add Subpage</a>
-          </div>
+      <div class="clone-elements" data-subitems="subpages">
+        <a href="#" class="add-item">Add Page</a>
+        <br /><br />
+        <div class="item">
+          <input type="text" name="pages[0][title]" placeholder="Page Title">
+          <a href="#" class="add-subitem">Add Subpage</a>
         </div>
       </div>
     </fieldset>
